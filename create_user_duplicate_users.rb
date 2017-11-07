1d04a2c828eaede32d64f3132396bd3924aee91c
Tina Heiligers <tinaheiligers@TW10216.local>
2017-11-07 15:58:37 -0700

    added a duplicate email user to create

---
+++ create_user_duplicate_users.rb
@@ -0,0 +1,28 @@
+require 'httparty'
+# log in as owner
+login_with_email = 'http://localhost:8080/admin/api/v1/session/loginWithEmail'
+login_response = HTTParty.post(login_with_email, body: {email: 'tstark@acme.com', password: 'password'})
+session_token = login_response['sessionToken']
+#  need to get a company because an owner does not have a company
+get_company_url = 'http://localhost:8080/admin/api/v1/companies'
+get_companies_response = HTTParty.get(get_company_url, headers: {'Authorization': "Bearer #{session_token}"})
+
+random_companyId = get_companies_response[0]['id']
+random_company = get_companies_response[0]
+# puts session_token
+puts "random_company #{random_company}"
+puts "random_company_id #{random_companyId}"
+
+
+#Create a new normal user
+create_new_user_path = 'http://localhost:8080/admin/api/v1/user'
+create_new_user_response = HTTParty.post(create_new_user_path, headers: {'Authorization': "Bearer #{session_token}"}, body: {firstName: 'Tina', lastName: 'Heiligers', email: 'tina.heiligers@tallwave.com', title: 'Developer', companyId: "#{random_companyId}"})
+puts create_new_user_response
+
+# Create a new admin user
+create_new_admin_response = HTTParty.post(create_new_user_path, headers: {'Authorization': "Bearer #{session_token}"}, body: {firstName: 'Tina', lastName: 'Heiligers', email: 'tina.heiligers@gmail.com', title: 'Developer', companyId: "#{random_companyId}", isAdmin: true})
+puts create_new_admin_response
+
+#  Try to create an admin using the new user's email address
+try_new_admin_response = HTTParty.post(create_new_user_path, headers: {'Authorization': "Bearer #{session_token}"}, body: {firstName: 'Tina', lastName: 'Heiligers', email: 'tina.heiligers@tallwave.com', title: 'Developer', companyId: "#{random_companyId}", isAdmin: true})
+#  Try to create a user using the new admins's email address
